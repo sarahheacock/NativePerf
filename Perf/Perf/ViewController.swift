671b06655293ca7bd6e032b006108671f33e3790
Sarah Heacock <seheacock@bellsouth.net>
2019-11-07 10:58:53 -0500

    swiftFund // Chapter 1 Swift Architecture

--- Perf/Perf/ViewController.swift
+++ Perf/Perf/ViewController.swift
@@ -6,30 +6,59 @@
 //  Copyright © 2019 Sarah Heacock. All rights reserved.
 //
 
+// imports Foundation too which allows you to speak to speak to NSString
+// without having the write Foundation.NSString
+// scope includes module, file, and curly braces
 import UIKit
+
+extension Int {
+    func sayHello() {
+        print("Hello, I'm \(self)")
+    }
+}
 
 class ViewController: UIViewController {
     var scrollView: UIScrollView!
     var imageView: UIImageView!
-//    var image: UIImage!
+    
+    func hello() {
+        print("hello!")
+    }
+    
+    // namespace does not provide security but convenience
+    // can only access Klass through ViewController.Klass
+    class Dog {
+        // private prevents fido.name = "Fido"
+        private var name = ""
+        // let name = "Lucy" would prevent changing at all
+        func bark() {
+            print("woof", self.name)
+        }
+        
+        func addName(name: String) {
+            self.name = name
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        scrollView = UIScrollView(frame: view.bounds)
+        // CHAPTER 1
+        // if you can extend it, it's an object
+        // primitives/scalar are really objects
+        // 1 is not a class or instance but an instance of a struct
+        1.sayHello() // outputs: "Hello, I'm 1"
+
+        // let is a constant
+        // variables cannot change type
+        // only function can contain executable code
+        // this works because runtime will always call viewDidLoad
         
-        guard let url = URL(string: "https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg") else { return }
-        let data = try? Data(contentsOf: url)
-        guard let image = UIImage(data: data!) else { return }
-
-        for n in 1...500 {
-            imageView = UIImageView(image: image)
-            let size: Int = 100
-            imageView.frame = CGRect(x: 0, y: n * size, width: size, height: size)
-            scrollView.addSubview(imageView)
-        }
-
-        view.addSubview(scrollView)
+        // create instance of Dog
+        let fido = Dog()
+        fido.bark()
+        fido.addName(name: "Fido")
+        fido.bark()
     }
 }
 
