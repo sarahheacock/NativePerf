df44c0ab21b61b828ba011eb5a7cff7e72eb128c
Sarah Heacock <seheacock@bellsouth.net>
2019-11-07 19:28:56 -0500

    swiftFund // Variables and Simple Types

---
+++ Perf/Perf/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Perf
+//
+//  Created by Sarah Heacock on 11/7/19.
+//  Copyright © 2019 Sarah Heacock. All rights reserved.
+//
+
+import Foundation
+
+// can call global variables in any file
+let globalVariable = "global"
+
--- Perf/Perf/ViewController.swift
+++ Perf/Perf/ViewController.swift
@@ -17,6 +17,7 @@ extension Int {
     }
 }
 
+
 class ViewController: UIViewController {
     var scrollView: UIScrollView!
     var imageView: UIImageView!
@@ -32,6 +33,17 @@ class ViewController: UIViewController {
         // private prevents fido.name = "Fido"
         private var name = ""
         // let name = "Lucy" would prevent changing at all
+        
+        // static prop
+        static let staticProperty = "staticProperty"
+        // instance prop
+        let instanceProp = "instanceProperty"
+        
+        // calling self.initFancy will not work because there is no instance yet
+        let fancy = {
+           return true
+        }()
+        
         func bark() {
             print("woof", self.name)
         }
@@ -107,6 +119,8 @@ class ViewController: UIViewController {
         let name = fido.addName("Fido")
         print(name)
         fido.bark()
+        print(Dog.staticProperty)
+        print(fido.instanceProp)
         
         // CHAPTER 2
         // external parameter names -> function can externalize names of params
@@ -183,6 +197,132 @@ class ViewController: UIViewController {
         let container = UIView(frame: CGRect(x: 200, y: 200, width: 100, height: 100))
         container.addSubview(button)
         view.addSubview(container)
+        
+        print(globalVariable)
+        
+        let timed : Bool = {
+            return true
+        }()
+        print(timed)
+        print(fido.fancy)
+        
+        var now: String {
+            get {
+                return Date().description
+            }
+            // set {
+            //     print(newValue)
+            // }
+            set (val) {
+                print(val)
+            }
+        }
+        now = "howdy"
+        print(now)
+        
+        // CHAPTER 3
+        var angle : CGFloat = 0 {
+            didSet {
+                // modify interface to match
+                // self.transform = CGAffineTransform(rotationAngle: self.angle)
+                print(oldValue)
+            }
+        }
+
+        // lazy initialization
+        // global, static variables are not initialized until needed
+        // instance props can be lazy if given word lazy
+        
+        // Singleton
+        // MyClass.sharedSingleton is initiated when called
+        // and is always the same instance
+        class MyClass {
+            static let sharedSingleton = MyClass()
+        }
+        
+        // lazy can refer to the instance
+        class MyView : UIView {
+            lazy var arrow = self.arrowImage() // legal
+            func arrowImage() -> Int {
+                // ... big image-generating code goes here ...
+                return 10
+            }
+        }
+        
+        // Bool struct
+        // nothing other than Bool can be coerced to true or false
+        class MyBool {
+            var p = false
+        }
+        let bool = MyBool()
+        print(bool.p)
+        bool.p.toggle()
+        print(bool.p)
+        
+        // Int struct
+        // Int.min Int.max
+        
+        // Double struct
+        // Double.infinity Double.pi
+        
+        // instantiation can do form of number coercion
+        let y = 3.8
+        let j = Int(y)
+        print(j) // 3, an Int
+        
+        let f = 0.1
+        var sum = 0.0
+        for _ in 0..<10 { sum += f }
+        let product = f * 10
+        let ok = sum == product // false
+        let ok2 = sum >= product.nextDown && sum <= product.nextUp // true
+        print(ok, ok2)
+        
+        // Foundation NSString properties come when using Swift string
+        let str = "hello world"
+        let s2 = str.capitalized // "Hello World"
+        print(s2)
+        
+        let filtered = s.filter{"aeiou".contains($0)} // "eo"
+        print(filtered)
+        
+        for ix in (1..<4).reversed() {
+            print(ix) // 3, 2, 1
+        }
+        
+        // tuples are purely swift and not compitible with cocoa or objective-C
+        // let pair = (1, "two")
+        let pair = (first:1, second:"Two")
+        print(pair)
+        
+        for (ix,c) in s.enumerated() {
+            print("character \(ix) is \(c)")
+        }
+        
+        // Optional object type (an enum) wraps another object of any type
+        // var stringMaybe = Optional("howdy")
+        var stringMaybe : String? = "howdy"
+        print(stringMaybe)
+        print(stringMaybe!) // ! grabs wrapped value, unwrapping nil causes error
+        if stringMaybe == nil {
+            print("it is empty") // prints
+        }
+        
+        // stringMaybe = nil
+        // let upper = stringMaybe!.uppercased() // optional chains
+        let upper = stringMaybe?.uppercased() // optional chains (optionally unwraps)
+        print(upper)
+        
+        // map and flatmap send message to wrapped without unwrapping
+        let maybeMap = stringMaybe.map{$0.uppercased()}
+        print(maybeMap)
+        print(maybeMap == "HOWDY")
+        
+        // flatmap unwraps before wrapping
+        let i = stringMaybe.flatMap{Int($0)}
+        print(i) // nil
+        
+        // optional because any objective C object can be nil
     }
 }
 
